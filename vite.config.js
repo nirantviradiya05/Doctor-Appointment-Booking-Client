80bb5b3b09ca4db4ae0b63e4344d84330540f5e5
Nirant <nirant05viradiya@gmail.com>
2025-08-28 23:00:38 -0400

    Describe what you changed

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Doctor-Appointment-Booking-Client/', // 👈 Base path for GitHub Pages project site
+  // Base public path when served in production.
+  // For GitHub Pages, set it to your repo name (including slashes).
+  base: '/Doctor-Appointment-Booking-Client/',
+
   plugins: [react()],
+
+  // Development server config (optional)
   server: {
-    port: 5173,  // Optional: specify your dev server port
+    port: 5173, // Specify your dev server port if needed
   },
 })
